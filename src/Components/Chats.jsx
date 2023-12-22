786ce76da55d44fd2ddeff902f396195963c451c
Hartz-byte <harshmail281199@gmail.com>
2023-12-22 11:30:06 +0530

    home start

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div>
+      <div className="userChat">
+        <img
+          src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
