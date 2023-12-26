822cdeb628cea8e6a2ebb91d328870cda1d16faa
Hartz-byte <harshmail281199@gmail.com>
2023-12-26 12:07:33 +0530

    web design

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
